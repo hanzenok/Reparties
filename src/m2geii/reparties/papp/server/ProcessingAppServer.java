585a9542339974fc7a76425894691d47799ad7ad
Mykhailo Ganza <hanzenok@gmail.com>
2015-11-22 21:06:44 +0100

    Fixed problem. Redefinition of CLASSPATH have to take place in the same terminal as rmiregistry is

--- src/m2geii/reparties/papp/server/ProcessingAppServer.java
+++ src/m2geii/reparties/papp/server/ProcessingAppServer.java
@@ -3,21 +3,28 @@ package m2geii.reparties.papp.server;
 import java.net.MalformedURLException;
 import java.rmi.Naming;
 import java.rmi.RemoteException;
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
+import java.rmi.server.UnicastRemoteObject;
+
+import m2geii.reparties.papp.inter.ProcessingAppInterface;
 
 public class ProcessingAppServer {
 
 	public static void main(String[] args) throws RemoteException, MalformedURLException {
 		
-//		if(System.getSecurityManager() == null) {
-//			
-//			System.setSecurityManager(new SecurityManager());
-//		}
+		if(System.getSecurityManager() == null) {
+			
+			System.setSecurityManager(new SecurityManager());
+		}
 		
 		System.out.println("Creation of object");
-		ProcessingApp pa = new ProcessingApp();
+		ProcessingAppInterface pa = new ProcessingApp();
+		ProcessingAppInterface stub = (ProcessingAppInterface) UnicastRemoteObject.exportObject(pa, 0):
 		
-		System.out.println("En attente");
-		Naming.rebind("123", pa);
+	    Registry registry = LocateRegistry.getRegistry();
+	    registry.rebind("123", stub);
+	    System.out.println("Server launched");
 
 	}
 }
