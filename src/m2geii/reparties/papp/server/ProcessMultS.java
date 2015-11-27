8f81c9c3a997733e136d2b0e96bbdcc3252222f6
GANZA Mykhailo <hanzenok@gmail.com>
2015-11-27 11:38:53 +0100

    Added gitignore file

--- src/m2geii/reparties/papp/server/ProcessMultS.java
+++ src/m2geii/reparties/papp/server/ProcessMultS.java
@@ -1,19 +1,45 @@
 package m2geii.reparties.papp.server;
 
+import m2geii.reparties.matrix.Matrix;
+import m2geii.reparties.matrix.MatrixException;
 import m2geii.reparties.queue.Process;;
 
 public class ProcessMultS extends Process{
-
-	ProcessMultS(int duration) {
+	
+	Matrix M;
+	float scal;
+	Matrix res;
+	
+	ProcessMultS(Matrix M, float scal, int duration) {
+		
 		super(duration);
-		// TODO Auto-generated constructor stub
+		
+		this.M = M;
+		this.scal = scal;
 	}
 
 	@Override
 	public void runProcess() {
 		
+		//simulation d'attente
+		try {Thread.sleep(duration*1000);} 
+		catch(InterruptedException e) {Thread.currentThread().interrupt();}
 		
+		//calcule
+		int i,j;
+		int n = M.rows();
+		int m = M.cols();
 		
+		Matrix res = M;
+		
+		for(i=0;i<n;i++){
+			
+			for(j=0;j<m;j++){
+				
+				try {res.setValue(i, j, M.getValue(i, j)*scal);} 
+				catch (MatrixException e) {e.printStackTrace();}
+			}
+		}
 	}
 	
 	
